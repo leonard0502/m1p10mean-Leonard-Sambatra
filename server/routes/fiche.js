bd75db8f84eb325cb583aefd14edc4c131639981
Léonard <lppleonard@gmail.com>
2023-01-13 12:27:02 +0300

    back-end

---
+++ server/routes/fiche.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const ficheRoutes = express.Router();
+const Fiche = require("../models/Fiche");
+
+ficheRoutes.post("/creerFiche", (req, res) => {
+  let { idVoiture, idUser, etat, dateFiche, reparation } = req.body;
+  const newFiche = new Fiche({ idVoiture: ObjectId(idVoiture), idUser : ObjectId(IdUser), etat : etat, dateFiche : dateFiche, reparation : reparation},);
+  newFiche
+    .save()
+    .then(() => {
+      res.json({
+        status: "EN COURS",
+        message: "Création de fiche effectué!",
+      });
+    })
+    .catch(() => {
+      res.json({
+        status: "ECHEC",
+        message: "Une erreur s'est produit lors de la création de la Fiche!",
+      });
+    });
+});
+ficheRoutes.get("/getFiche", (req, res) => {
+  Fiche.find({})
+    .then((result) => {
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({
+          status: "ECHEC",
+          message: "Aucun fiche",
+        });
+      }
+    })
+    .catch(() => {
+      res.json({
+        status: "ECHEC",
+        message: "Une erreur s'est produit lors de l'obtention des fiches!",
+      });
+    });
+});
+ficheRoutes.get("/getFicheById/", (req, res) => {
+    Fiche.find({_id : ObjectId(req.query.id)})
+    .populate("idVoiture")
+    .populate("idUser")
+      .then((result) => {
+        if (result.length > 0) {
+          res.json(result);
+        } else {
+          res.json({
+            status: "ECHEC",
+            message: "Aucun fiche",
+          });
+        }
+      })
+      .catch(() => {
+        res.json({
+          status: "ECHEC",
+          message: "Une erreur s'est produit lors de l'obtention des fiches!",
+        });
+      });
+  });
+module.exports = ficheRoutes;
---
+++ server/routes/user.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const userRoutes = express.Router();
+const User = require("../models/User");
+
+userRoutes.get("/verifier", async (req, res) => {
+  let {email, mdp } = req.query;
+  const user = await User.findOne({ email: email });
+  if (user) {
+    const validPassword = await bcrypt.compare(mdp, user.mdp);
+    if (validPassword) {
+      res.json(user);
+    } else {
+      res.json({
+        status: "Erreur",
+        message: "Votre mot de pass ou/et email est incorrect!",
+      });
+    }
+  } else {
+    res.json({
+      status: "Erreur",
+      message: "Votre mot de pass ou/et email est incorrect!!",
+    });
+  }
+  });
+
+userRoutes.post("/creerUser", (req, res) => {
+    let { type, nom, prenom, email, mdp, contact } = req.body;
+    console.log(req.body)
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(mdp, salt);
+    const newUser = new User({ type: type, nom:nom, prenom:prenom, email: email, mdp : hash, contact:contact});
+    newUser
+      .save()
+      .then(() => {
+        res.json({
+          status: "EN COURS",
+          message: "Création d' user effectué!",
+        });
+      })
+      .catch(() => {
+        res.json({
+          status: "ECHEC",
+          message: "Une erreur s'est produit lors de la création d' user'!",
+        });
+      });
+  });
+module.exports = userRoutes;
