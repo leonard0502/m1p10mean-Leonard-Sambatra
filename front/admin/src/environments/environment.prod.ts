3d549aa9b444a05d4bc3ef220fcfdf301c92417a
Léonard <lppleonard@gmail.com>
2023-01-28 09:00:27 +0300

    config

--- front/admin/src/environments/environment.prod.ts
+++ front/admin/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  back : 'https://garage-leo-sambatra-api.onrender.com',
+  Depense_base_url : 'https://garage-leo-sambatra-api.onrender.com/depense',
+  Voiture_base_url : 'https://garage-leo-sambatra-api.onrender.com/voiture',
+  Fiche_base_url : 'https://garage-leo-sambatra-api.onrender.com/fiche',
+  User_base_url : 'https://garage-leo-sambatra-api.onrender.com/user',
 };
